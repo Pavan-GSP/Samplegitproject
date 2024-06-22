c416482075411abceccaf4a58179493e84347e2f
root <root@ip-172-31-34-176.eu-north-1.compute.internal>
2024-06-22 07:38:44 +0000

    add i+1
    wq
    exit

--- sample.java
+++ sample.java
@@ -1,6 +1,6 @@
 class Test{
     public static void main(String[] args)
     {
-        System.out.println("My First Java Program and it's running fine");
+        System.out.println("My First Java Program and it's running fine",i+1);
     }
 }
