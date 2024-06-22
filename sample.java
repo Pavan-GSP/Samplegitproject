a5690a086e39858c28778225e65f6a584dccbf1e
root <root@ip-172-31-34-176.eu-north-1.compute.internal>
2024-06-22 10:21:51 +0000

    update 1 time

--- sample.java
+++ sample.java
@@ -1,10 +1,6 @@
 class Test{
     public static void main(String[] args)
     {
-<<<<<<< HEAD
         System.out.println("My First Java Program and it's running fine",i+1);
-=======
-        System.out.println("My First Java Program,i++");
->>>>>>> new1
     }
 }
